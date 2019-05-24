9d7db5ecfa6c93879adeeb21ed273da3e9f3a153
冯飞杨 <fengfeiyang@haixue.com>
2019-05-24 18:07:36 +0800

    demo for study

---
+++ Audio-Video-Demo/first_problem/src/main/java/com/person/ermao/first_problem/ImageErmaoView.java
@@ -0,0 +1,22 @@
+package com.person.ermao.first_problem;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by 冯飞杨 on 2019/5/24.
+ */
+public class ImageErmaoView extends View {
+    public ImageErmaoView(Context context) {
+        super(context);
+    }
+
+    public ImageErmaoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ImageErmaoView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ Audio-Video-Demo/first_problem/src/main/java/com/person/ermao/first_problem/ShowImageActivity.java
@@ -0,0 +1,7 @@
+package com.person.ermao.first_problem;
+
+/**
+ * Created by 冯飞杨 on 2019/5/24.
+ */
+public class ShowImageActivity {
+}
